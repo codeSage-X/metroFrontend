4b9bf95941ae7b5b44ba478e62d3799450c83527
Wisdom john ikoi <wisdomjohnikoi@gmail.com>
2025-10-31 14:14:52 +0100

    metro front-end ui ready

--- metro/lib/auth-service.tsx
+++ metro/lib/auth-service.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api/auth"
+const BASE_URL = "https://metrocare-eigg.onrender.com/api/auth/"
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
